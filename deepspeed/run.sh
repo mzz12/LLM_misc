346ac65442df0984a9f5408c594776639e2adac0
mzz12 <153173356+mzz12@users.noreply.github.com>
2023-12-30 21:50:42 +0800

    Update run.sh

--- deepspeed/run.sh
+++ deepspeed/run.sh
@@ -3,17 +3,17 @@
 deepspeed main.py \
    --data_path yitingxie/rlhf-reward-datasets \
    --data_split 2,4,4 \
-   --model_name_or_path ./config.json \
+   --model_name_or_path facebook/opt-1.3b \
    --per_device_train_batch_size 1 \
    --max_seq_len 512 \
    --learning_rate 9.65e-6 \
    --weight_decay 0. \
    --num_train_epochs 1 \
    --gradient_accumulation_steps 1 \
-   --gradient_checkpointing \   
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --zero_stage 3 \
    --offload \
+   --gradient_checkpointing \   
    --deepspeed \
    --output_dir ./output \
